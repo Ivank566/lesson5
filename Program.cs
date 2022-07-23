4037bac1dfe9b5fc058be7d6a5347ae056d10ad1
Bles <Bles@DESKTOP-6VBEV3N>
2022-07-23 16:28:01 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace lesson5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("vvedit' pershe chyslo:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("vvedit' druge chyslo:");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Сума чисел що знаходяться між ними");
+            int sum = 0;
+            for (int i= a +1; i<b; i++ )
+            {
+                Console.WriteLine(sum += i);
+            }
+            
+
+            Console.WriteLine("Введите первое число:");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Непарні числа що знаходяться між ними:");
+            for(int j = x+1; j<y; j++)
+            {
+                if (j % 2 != 0)
+                {
+                    Console.WriteLine(j);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
